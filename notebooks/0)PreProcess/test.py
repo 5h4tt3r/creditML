edf008bf480e5151706f3523081353023e85a229
5h4tt3r <x5h4tt3rx@gmail.com>
2025-06-08 14:19:52 -0600

    gurick inital branch working on cleaning

--- notebooks/0)PreProcess/test.py
+++ notebooks/0)PreProcess/test.py
@@ -26,6 +26,12 @@ df.head(20)
 
 # %% Data For Missing Values
 df.isnull().sum().sort_values(ascending=False)
+# %% DROPPING AFTER THE NULL CHECK
+# mech_zipcode shows a lot of missing values so we need to drop it
+# Unamed: 0 is just the row index
+drop_cols = ['Unnamed: 0', 'merch_zipcode']
+df.drop(columns=drop_cols, inplace=True)
+print("Dropped columns:", drop_cols)
 
 # %% Data Types
 print(set(df.dtypes.to_list()),"\n")
@@ -37,8 +43,20 @@ df.describe()
 # %% Duplicate Data
 duplicates = df.duplicated().sum()
 print(f"Number of duplicates: {duplicates}")
-df['Time'].describe()
-df['Time'].value_counts()
-df['Time'].value_counts().plot(kind='bar')
-df['Time'].value_counts().plot(kind='bar').set_title('Time Distribution')
-df['Time'].value_counts().plot(kind='bar').set_title('Time Distribution').set_xlabel('Time').set_ylabel('Count')
+
+# %% Duplicate Rows
+dup_count = df.duplicated().sum()
+print(f"Duplicate rows: {dup_count}")
+if dup_count > 0:
+    df.drop_duplicates(inplace=True)
+    print("Dropped duplicate rows")
+
+
+# %% 6) OUTLIER DETECTION (amt example)
+#  - Flag txns above 99th percentile
+q99 = df['amt'].quantile(0.99)
+#creating a new column where encoding -> where 1 indicates high value transaction outlier and 0 indicates normal transaction
+df['amt_outlier'] = (df['amt'] > q99).astype(int)
+#99% of all transactions are below this value which is $545.99 so only 1 % of transactions are higher than this
+# might pass this encoding into a model to predict fraud (behaviour at large spending values)
+print(f"99th percentile of amt = {q99:.2f}; flagged amt_outlier")
